c193c1115efd6b544365d8328800089469f18317
Richard W. Norton <rwtnorton@gmail.com>
2018-10-02 01:18:20 -0700

    run first sort

--- src/mungify/main.clj
+++ src/mungify/main.clj
@@ -1,6 +1,16 @@
 (ns mungify.main
-  (:gen-class))
+  (:gen-class)
+  (:require [mungify.person :as p]
+            [mungify.person.parser :as parser]
+            [mungify.person.format :as fmt]
+            [mungify.person.sort-by-gender-last-name :as gln.sorter])
+  (:import (java.io BufferedReader)))
 
 (defn -main
   [& args]
-  (println "ohai!"))
+  (let [peeps (map-indexed (fn [i s]
+                             (assoc (parser/string->person s) :id (inc i)))
+                           (line-seq (BufferedReader. *in*)))]
+    (println "== Sorted by gender then by last name ascending")
+    (doseq [p (gln.sorter/sort-people peeps)]
+      (println (fmt/person->string p)))))

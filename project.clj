9b65933a1fa28c9b6c735a4f4bca2801aedb59e8
Richard W. Norton <rwtnorton@gmail.com>
2018-10-01 20:26:00 -0700

    grab linters and the like

--- project.clj
+++ project.clj
@@ -3,10 +3,15 @@
   :url "https://github.com/rwtnorton/mungify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [
-            [lein-cloverage "1.0.13"] ;; really should be in ~/.lein/profiles.clj
-                                      ;; but keeping it here for portability
-            ]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.2.176"]
+                 [orchestra "2018.09.10-1"]
+                 [clj-time "0.14.4"]]
+  :plugins [;; These really should be in ~/.lein/profiles.clj
+            ;; but keeping them here for portability.
+            [lein-cloverage "1.0.13"]
+            [lein-cljfmt "0.6.1"]
+            [lein-kibit "0.1.6"]
+            [jonase/eastwood "0.2.9"]]
   :main mungify.main)
 

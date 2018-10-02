9c2234b2a3f627ed0c83bdb94aa54b284c55a601
Richard W. Norton <rwtnorton@gmail.com>
2018-10-02 01:22:27 -0700

    sort by last name desc

--- src/mungify/main.clj
+++ src/mungify/main.clj
@@ -4,7 +4,8 @@
             [mungify.person.parser :as parser]
             [mungify.person.format :as fmt]
             [mungify.person.sort-by-gender-last-name :as gln.sorter]
-            [mungify.person.sort-by-birthdate :as bd.sorter])
+            [mungify.person.sort-by-birthdate :as bd.sorter]
+            [mungify.person.sort-by-last-name :as ln.sorter])
   (:import (java.io BufferedReader)))
 
 (defn -main
@@ -12,9 +13,12 @@
   (let [peeps (map-indexed (fn [i s]
                              (assoc (parser/string->person s) :id (inc i)))
                            (line-seq (BufferedReader. *in*)))]
-    (println "== Sorted by gender then by last name ascending")
+    (println "==== Sorted by gender then by last name ascending")
     (doseq [p (gln.sorter/sort-people peeps)]
       (println (fmt/person->string p)))
-    (println "== Sorted by birthdate ascending")
+    (println "==== Sorted by birthdate ascending")
     (doseq [p (bd.sorter/sort-people peeps)]
+      (println (fmt/person->string p)))
+    (println "==== Sorted by last name descending")
+    (doseq [p (ln.sorter/sort-people peeps)]
       (println (fmt/person->string p)))))

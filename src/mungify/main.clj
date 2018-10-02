d5a6b9ff33294e0ae2547c88061a31267e8a42ca
Richard W. Norton <rwtnorton@gmail.com>
2018-10-01 17:55:06 -0700

    switch from core to main for driver

---
+++ src/mungify/main.clj
@@ -0,0 +1,6 @@
+(ns mungify.main
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (println "ohai!"))
--- test/mungify/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns mungify.core-test
-  (:require [clojure.test :refer :all]
-            [mungify.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
